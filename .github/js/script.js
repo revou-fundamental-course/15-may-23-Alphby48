3fe5f10cd7fc391f3adea970f4b3e966f0f49e06
Alphby48 <hazerdmobile@gmail.com>
2023-05-20 15:59:56 +0700

    add some javascript

---
+++ .github/js/script.js
@@ -0,0 +1,6 @@
+function hitungLuas() {
+  var sisiLuas = document.getElementById("sisi-luas").value;
+  document.getElementsByClassName("result-luas").console.log(sisiLuas);
+  document.getElementById("output-luas").innerHTML =
+    "<span>" + sisiLuas + "</span>";
+}
